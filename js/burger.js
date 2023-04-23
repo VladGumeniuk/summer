f53a490f2641fccf6253b7797f4ce3d912ba2140
VladGumeniuk <76571048+VladGumeniuk@users.noreply.github.com>
2023-04-23 18:52:24 +0300

    Add files via upload

--- js/burger.js
+++ js/burger.js
@@ -4,15 +4,15 @@ let back = document.querySelector("body");
 let header__list = document.querySelector(".header__list");
 
 header__burger.onclick = function () {
-  header__burger.classList.toggle("active");
-  header_menu.classList.toggle("active");
+  header__burger.classList.toggle("actived");
+  header_menu.classList.toggle("actived");
   //   back.classList.toggle("lock");
 };
 
 header__list.onclick = function () {
-  header_menu.classList.remove("active");
-  header__list.classList.remove("active");
-  header__burger.classList.remove("active");
+  header_menu.classList.remove("actived");
+  header__list.classList.remove("actived");
+  header__burger.classList.remove("actived");
   //   back.classList.classList.remove("lock");
   //   header__list.classList.remove("lock");
 };
